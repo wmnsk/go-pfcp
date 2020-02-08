9e75c4b91a901d5795153e14656462508461bb95
Yoshiyuki Kurauchi <ahochauwaaaaa@gmail.com>
2020-02-08 18:41:52 +0900

    add EndTime IE

---
+++ ie/end-time.go
@@ -0,0 +1,29 @@
+// Copyright 2019-2020 go-pfcp authors. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+
+package ie
+
+import (
+	"encoding/binary"
+	"io"
+	"time"
+)
+
+// NewEndTime creates a new EndTime IE.
+func NewEndTime(ts time.Time) *IE {
+	u64sec := uint64(ts.Sub(time.Date(1900, time.January, 1, 0, 0, 0, 0, time.UTC))) / 1000000000
+	return newUint32ValIE(EndTime, uint32(u64sec))
+}
+
+// EndTime returns EndTime in time.Time if the type of IE matches.
+func (i *IE) EndTime() (time.Time, error) {
+	if i.Type != EndTime {
+		return time.Time{}, &InvalidTypeError{Type: i.Type}
+	}
+
+	if len(i.Payload) < 4 {
+		return time.Time{}, io.ErrUnexpectedEOF
+	}
+	return time.Unix(int64(binary.BigEndian.Uint32(i.Payload[0:4])-2208988800), 0), nil
+}
