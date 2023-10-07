91b279bae2cafea71bd261bb3b51fd9226700631
Yoshiyuki Kurauchi <ahochauwaaaaa@gmail.com>
2023-10-07 15:15:37 +0200

    Handle Additional CP Function Features
    Co-authored-by: Louis Royer <infos.louis.royer@gmail.com>

--- ie/cp-function-features.go
+++ ie/cp-function-features.go
@@ -4,18 +4,22 @@
 
 package ie
 
+import "io"
+
 // NewCPFunctionFeatures creates a new CPFunctionFeatures IE.
-func NewCPFunctionFeatures(features uint8) *IE {
-	return newUint8ValIE(CPFunctionFeatures, features)
+func NewCPFunctionFeatures(features ...uint8) *IE {
+	return New(CPFunctionFeatures, features)
 }
 
-// CPFunctionFeatures returns CPFunctionFeatures in uint8 if the type of IE matches.
-func (i *IE) CPFunctionFeatures() (uint8, error) {
+// CPFunctionFeatures returns CPFunctionFeatures in []byte if the type of IE matches.
+func (i *IE) CPFunctionFeatures() ([]byte, error) {
 	if i.Type != CPFunctionFeatures {
-		return 0, &InvalidTypeError{Type: i.Type}
+		return nil, &InvalidTypeError{Type: i.Type}
 	}
-
-	return i.ValueAsUint8()
+	if len(i.Payload) < 1 {
+		return nil, io.ErrUnexpectedEOF
+	}
+	return i.Payload, nil
 }
 
 // HasLOAD reports whether an IE has LOAD bit.
@@ -40,4 +44,52 @@ func (i *IE) HasOVRL() bool {
 	}
 
 	return has2ndBit(i.Payload[0])
+}
+
+// HasARDR reports whether an IE has ARDR bit.
+func (i *IE) HasARDR() bool {
+	if i.Type != CPFunctionFeatures {
+		return false
+	}
+	if len(i.Payload) < 1 {
+		return false
+	}
+
+	return has7thBit(i.Payload[0])
+}
+
+// HasUIAUR reports whether an IE has UIAUR bit.
+func (i *IE) HasUIAUR() bool {
+	if i.Type != CPFunctionFeatures {
+		return false
+	}
+	if len(i.Payload) < 1 {
+		return false
+	}
+
+	return has8thBit(i.Payload[0])
+}
+
+// HasPSUCC reports whether an IE has PSUCC bit.
+func (i *IE) HasPSUCC() bool {
+	if i.Type != CPFunctionFeatures {
+		return false
+	}
+	if len(i.Payload) < 2 {
+		return false
+	}
+
+	return has1stBit(i.Payload[1])
+}
+
+// HasRPGUR reports whether an IE has RPGUR bit.
+func (i *IE) HasRPGUR() bool {
+	if i.Type != CPFunctionFeatures {
+		return false
+	}
+	if len(i.Payload) < 2 {
+		return false
+	}
+
+	return has2ndBit(i.Payload[1])
 }
